fe502f31ea9d781e41841b1042cc3492dd5f1465
Brayan <bd432@scarletmail.rutgers.edu>
2023-11-01 11:50:14 -0400

    new file names

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -1,4 +1,4 @@
-package hellofx;
+package chess;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Chess extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
--- src/chess/Controller.java
+++ src/chess/Controller.java
@@ -1,4 +1,4 @@
-package hellofx;
+package chess;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
