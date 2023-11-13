5c8f475bb65265662b96d1cff28c70d27cffbe47
Brayan <bd432@scarletmail.rutgers.edu>
2023-11-13 09:35:51 -0500

    this should work now when you test it

--- src/photos/view/editPictureController.java
+++
@@ -1,5 +0,0 @@
-package photos.view;
-
-public class EditPictureController {
-    
-}
--- src/photos/view/loginController.java
+++ src/photos/view/loginController.java
@@ -4,8 +4,9 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
@@ -25,7 +26,8 @@ public class loginController {
     @FXML
     private TextField username;
 
-    @FXML TextField password;
+    @FXML 
+    private TextField password;
 
     @FXML
     private Button loginButton;
@@ -41,7 +43,7 @@ public class loginController {
         String pass = password.getText();
         if(allUsers.searchForUser(user) != null && allUsers.searchForUser(user).getPassword().equals(pass)){
             if(user.equals("admin")){
-
+                loginForAdmin(event,allUsers);
             }
                 else{
                     loginForUsers(event,allUsers.searchForUser(user));
@@ -54,13 +56,40 @@ public class loginController {
     }
     
     public void loginForUsers(ActionEvent event, User user) throws Exception{
-       //AlbumController albumController = new AlbumController(user);
+        albumController album = new albumController(user);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/photos/view/album.fxml"));
+        loader.setController(album);
+        root = loader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setTitle("Albums");
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
 
     }
+
+    public void loginForAdmin(ActionEvent event,AllUsers userList) throws Exception{
+        adminController admin = new adminController(userList);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/photos/view/admin.fxml"));
+        loader.setController(admin);
+        root = loader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setTitle("Admin");
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+
+    }
+
+
     public void initialize() throws IOException, ClassNotFoundException {
         if(allUsers.isEmpty()){
             User newUser = new User("stock","stock");
+            User admin = new User("admin","admin");
             allUsers.addUser(newUser);
+            allUsers.addUser(admin);
             allUsers.saveData(); // Use the saveData method
         } else {
             allUsers = AllUsers.getAllUsers();
